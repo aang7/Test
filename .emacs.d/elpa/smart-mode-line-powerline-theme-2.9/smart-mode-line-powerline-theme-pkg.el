f08e1ff47e7af64e82801578210858051e4887cc
Abel <aang.drummer@gmail.com>
2016-06-06 12:26:39 -0500

    Files on .emacs

---
+++ .emacs.d/elpa/smart-mode-line-powerline-theme-2.9/smart-mode-line-powerline-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "smart-mode-line-powerline-theme" "2.9" "smart-mode-line theme that mimics the powerline appearance." (quote ((emacs "24.3") (powerline "2.2") (smart-mode-line "2.5"))))
