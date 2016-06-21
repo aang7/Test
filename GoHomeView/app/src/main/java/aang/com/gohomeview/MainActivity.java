fafa9b46a3bffbba18c85ba9d8bf5aa7699c1ae8
aang7 <aang.drummer@gmail.com>
2016-06-21 11:11:42 -0500

    Test Finish

--- GoHomeView/app/src/main/java/aang/com/gohomeview/MainActivity.java
+++ GoHomeView/app/src/main/java/aang/com/gohomeview/MainActivity.java
@@ -1,47 +1,33 @@
 package aang.com.gohomeview;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewDebug;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CustomGauge gauge;
+    private GoHomeGauge gauge;
     private int valor = 0;
     private int distance = 0;
-
-    private AlphaAnimation animation;
     private TextView showValor;
-
     private TextView showDistance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gauge = (CustomGauge) findViewById(R.id.gaugeAtLocation);
-        /*bIncrement = (Button) findViewById(R.id.increment);
-        bDecrement = (Button) findViewById(R.id.decrement);*/
+        gauge = (GoHomeGauge) findViewById(R.id.gaugeAtLocation);
+
         showValor = (TextView) findViewById(R.id.showValor);
         showDistance = (TextView) findViewById(R.id.showDistance);
         gauge.setValue(valor, distance);
         showValor.setText(Integer.toString(valor));
         showDistance.setText(Integer.toString(distance));
 
-
-        /*AANG*/
-        animation = new AlphaAnimation(0.0f, 1.0f);
-        animation.setDuration(1000);
-        animation.setStartOffset(50);
-        animation.setFillAfter(true);
-        animation.setRepeatMode(Animation.INFINITE);
-
+        //Creo el Runnable
+        gauge.goAnimation();
 
     }
 
@@ -49,14 +35,14 @@ public class MainActivity extends AppCompatActivity {
         distance +=5;
         gauge.setValue(valor, distance);
         showDistance.setText(Integer.toString(distance));
-        gauge.startAnimation(animation);
+        //gauge.startAnimation(animation);
     }
 
     public void DecrementaD(View view){
         distance -=5;
         gauge.setValue(valor, distance);
         showDistance.setText(Integer.toString(distance));
-        gauge.clearAnimation();
+        //gauge.clearAnimation();
     }
 
     public void Incrementa(View view) {
